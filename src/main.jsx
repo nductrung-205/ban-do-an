db73a0c853629c5cfae09700067860bb9dae9409
Duc Trung <nductrung205@gmail.com>
2025-10-29 15:04:36 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ProductProvider } from './context/ProductProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ProductProvider>
-      <App />
-    </ProductProvider>
+  <StrictMode>    
+    <App />
   </StrictMode>,
 )
